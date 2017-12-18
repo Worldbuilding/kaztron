961c008765ec44fabdfbd84f5f321887884dea25
JDY <jdy@cxcf.me>
2017-12-18 14:10:00 -0500

    Minor fix + requirements
    Changed deprecated alias for google api + added requirements.txt

--- showcaser.py
+++ showcaser.py
@@ -4,7 +4,7 @@ import httplib2
 import os
 import random
 
-from apiclient import discovery
+from googleapiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
