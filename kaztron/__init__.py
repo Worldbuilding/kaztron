19cf19c004e42cc133f19df7a4671855e56075f1
Marc-Alexandre Chan <laogeodritt@arenthil.net>
2018-02-12 13:21:29 -0500

    Update to new github central repo

--- kaztron/__init__.py
+++ kaztron/__init__.py
@@ -14,9 +14,9 @@ bot_info = {
         "x Various bugfixes - see git history",
     "links": OrderedDict()
 }
-bot_info["links"]["Manual"] = "https://github.com/Kazandaki/KazTron/wiki"
-bot_info["links"]["GitHub"] = "https://github.com/Kazandaki/KazTron"
-bot_info["links"]["Bugs/Issues"] = "https://github.com/Kazandaki/KazTron/issues"
+bot_info["links"]["Manual"] = "https://github.com/Worldbuilding/KazTron/wiki"
+bot_info["links"]["GitHub"] = "https://github.com/Worldbuilding/KazTron"
+bot_info["links"]["Bugs/Issues"] = "https://github.com/Worldbuilding/KazTron/issues"
 bot_info["links"]["Spotlight Apps"] = "https://docs.google.com/spreadsheets/d/1YSwx6AJFfOEzIwTAeb71YXEeM0l34mUt6OvyhxTwQis/edit?usp=sharing"
 
 cfg_defaults = {
