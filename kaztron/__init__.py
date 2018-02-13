4976fe63d1527854b3ce45b760b10298a2fb25eb
Marc-Alexandre Chan <laogeodritt@arenthil.net>
2018-02-13 13:09:57 -0500

    Changelog update

--- kaztron/__init__.py
+++ kaztron/__init__.py
@@ -6,12 +6,11 @@ __version__ = "1.3b2"
 bot_info = {
     "version": __version__,
     "changelog":
-        "+ Modnotes\n"
-        "+ Spotlight queue management feature\n"
-        "+ Spotlight Host role management\n"
-        "+ Refactor of all command UI and usability improvements\n"
-        "+ Improved logging facilities\n"
         "+ Internal rewrite for maintainability & reliability\n"
+        "+ Modnotes\n"
+        "+ Spotlight queue management and Host role management features\n"
+        "+ Refactor of command UI and usability improvements\n"
+        "+ Improved logging facilities\n"
         "- Various bugfixes - see git history",
     "links": OrderedDict()
 }
