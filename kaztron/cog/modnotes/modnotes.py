fc6c0a15919d0a743309ff6a238974746cf29706
Marc-Alexandre Chan <laogeodritt@arenthil.net>
2019-07-27 01:27:14 -0400

    fix notes pagination (fix #220)

--- kaztron/cog/modnotes/modnotes.py
+++ kaztron/cog/modnotes/modnotes.py
@@ -210,9 +210,9 @@ class ModNotes(KazCog):
         db_group = c.query_user_group(db_user)
         db_records = c.query_user_records(db_group)
 
-        records_pages = Pagination(db_records, self.NOTES_PAGE_SIZE, True)
+        records_pages = Pagination(db_records, self.NOTES_PAGE_SIZE, align_end=True)
         if page is not None:
-            records_pages.page = max(1, min(records_pages.total_pages, page))
+            records_pages.page = max(0, min(records_pages.total_pages - 1, page - 1))
 
         await self.show_record_page(
             ctx.message.channel,
