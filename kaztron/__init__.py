c2327363be5c20bb117a93656b2e11745b9d4627
Marc-Alexandre Chan <laogeodritt@arenthil.net>
2020-09-22 03:39:00 -0400

    bump version to 2.3.2

--- kaztron/__init__.py
+++ kaztron/__init__.py
@@ -4,7 +4,7 @@ from .kazcog import KazCog
 from .scheduler import Scheduler, TaskInstance, task
 
 __release__ = "2.3"  # release stream, usually major.minor only
-__version__ = "2.3.1"
+__version__ = "2.3.2"
 
 bot_info = {
     "version": __version__,
