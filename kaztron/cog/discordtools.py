5e758b644ad517430b3c66351eca621a31ec1ef8
Marc-Alexandre Chan <laogeodritt@arenthil.net>
2020-10-11 04:20:12 -0400

    [discordtools] Add .kaztime - Fix #292 (for now - see issue)

--- kaztron/cog/discordtools.py
+++ kaztron/cog/discordtools.py
@@ -1,9 +1,11 @@
 import logging
+from datetime import datetime
 
 from discord.ext import commands
 
 from kaztron import KazCog
 from kaztron.utils.checks import mod_only, mod_channels
+from kaztron.utils.datetime import format_datetime
 from kaztron.utils.discord import channel_mention
 
 logger = logging.getLogger(__name__)
@@ -34,6 +36,20 @@ class DiscordTools(KazCog):
         """
         await self.send_message(ctx.message.channel, ctx.message.author.mention +
             " Your ID is: " + ctx.message.author.id)
+
+    @commands.command(pass_context=True, ignore_extra=True, no_pm=False, aliases=['now', 'time'])
+    async def kaztime(self, ctx: commands.Context):
+        """!kazhelp
+
+        description: "Gets the current KazTron Time (UTC, GMT). Can be used to help convert
+            community programme schedules to your timezone."
+        parameters: []
+        examples:
+            - command: .kaztime
+              description: Shows the current time.
+        """
+        await self.send_message(ctx.message.channel, ctx.message.author.mention +
+            "Current KazTime: {} UTC".format(format_datetime(datetime.utcnow(), seconds=True)))
 
     @commands.command(pass_context=True, ignore_extra=True, no_pm=False)
     @mod_only()
